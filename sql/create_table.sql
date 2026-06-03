2ce6770fdb9fdd7d0ed291909649f106b2790780
FeiWuSama <1825578184@qq.com>
2026-06-03 15:25:06 +0800

    fix：删除创建表的sql文件的部分无用内容

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -44,16 +44,3 @@ create table app
     INDEX idx_appName (appName),         -- 提升基于应用名称的查询性能
     INDEX idx_userId (userId)            -- 提升基于用户 ID 的查询性能
 ) comment '应用' collate = utf8mb4_unicode_ci;
-
-curl -X POST "http://localhost:8123/api/user/login" ^
--H "Content-Type: application/json" ^
--d "{\"userAccount\":\"1825578184\",\"userPassword\":\"815256031\"}" ^
--c cookies.txt
-
-curl -G "http://localhost:8123/api/app/chat/gen/code" ^
-  --data-urlencode "appId=123456" ^
-  --data-urlencode "message=我需要一个简单的任务记录工具网站" ^
-  -H "Accept: text/event-stream" ^
-  -H "Cache-Control: no-cache" ^
-  -b cookies.txt ^
-  --no-buffer
